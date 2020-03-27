a813298e9e9f3e3ed567a067e8f548f996cd682f
PRABHAT DIXIT <Prabhatdixit@Prabhats-Macbook-Pro-2.local>
2020-03-26 22:21:31 -0700

    Finetuning expense summary

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -8,15 +8,16 @@ import getExpensesTotal from '../selectors/expenses-total';
 
 const ExpenseSummary = (props) => (
         <div>
-        <h3>{props.expenses.length > 0 && `Showing ${props.expenses.length} expenses totalling $ ${props.total}` }</h3>
+        <h3>{props.expenses.length > 0 && `Showing ${props.expenses.length} expenses totalling ${numeral(props.total).format('$0,0.00')}` }</h3>
             
         </div>
 )
 
 const mapStateToProps = (state) => {
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
     return {
-        expenses: getVisibleExpenses(state.expenses, state.filters),
-        total:getExpensesTotal(getVisibleExpenses(state.expenses, state.filters))
+        expenses: visibleExpenses,
+        total:getExpensesTotal(visibleExpenses)
         
     };
 }
