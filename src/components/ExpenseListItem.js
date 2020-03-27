2ae5f41e997174042f7963d92a92e0e693712ec5
PRABHAT DIXIT <Prabhatdixit@Prabhats-Macbook-Pro-2.local>
2020-03-26 19:42:03 -0700

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,12 +2,18 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import { removeExpense } from '../actions/expenses';
+import moment from 'moment';
+import numeral from 'numeral';
  
 //could be deconstructed to (description, amount, createdAt) if using spread on ExpenseListItem call in ExpenseList
 const ExpenseListItem = (props) => (
     <div>
         <Link to={`/edit/${props.id}`}><h3>Description:{props.description}</h3></Link>
-        <p>{props.amount} - {props.createdAt}</p>
+        <p>
+        {numeral(props.amount).format('$0,0.00')}   -   
+        {moment(props.createdAt).format('MMM Do YYYY')}
+        
+        </p>
 
         
     </div>
