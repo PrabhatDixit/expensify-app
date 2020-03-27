182ec02bb59833da446d76db946ee65d0e663467
PRABHAT DIXIT <Prabhatdixit@Prabhats-Macbook-Pro-2.local>
2020-03-27 01:48:52 -0700

    Fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV | 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 if (process.env.NODE_ENV === 'test'){
     
